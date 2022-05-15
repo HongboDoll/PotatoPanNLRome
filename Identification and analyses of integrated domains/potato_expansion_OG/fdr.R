ce02e83e4423fa09dbd6c58f5050364aaaf9baac
HongboDoll <101861734+HongboDoll@users.noreply.github.com>
2022-05-15 18:56:59 +0800

    Delete Identification and analyses of integrated domains/potato_expansion_OG directory

--- Identification and analyses of integrated domains/potato_expansion_OG/fdr.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-argv<-commandArgs(TRUE)
-d <- read.table(argv[1],header=F)
-
-e <- p.adjust(d$V2,method='fdr',length(d$V2))
-
-write.table(e,argv[2],row.names = FALSE,col.names = FALSE,quote = FALSE,sep='\n')
-
