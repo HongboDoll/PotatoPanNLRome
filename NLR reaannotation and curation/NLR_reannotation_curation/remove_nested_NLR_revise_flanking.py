7dace40351b81cd891497cbd96538e54b89dc594
HongboDoll <101861734+HongboDoll@users.noreply.github.com>
2022-10-02 13:41:01 +0800

    Add files via upload

--- NLR reaannotation and curation/NLR_reannotation_curation/remove_nested_NLR_revise_flanking.py
+++ NLR reaannotation and curation/NLR_reannotation_curation/remove_nested_NLR_revise_flanking.py
@@ -3,7 +3,8 @@
 import sys,math
 
 i1 = sys.stdin  # grep -v '#' Heinz.NLR.gff3 | sort -k1,1 -k4,4n -k5,5n |
-i2 = int(sys.argv[1])  # left/right flanking length, 2000
+i2 = int(sys.argv[1])  # left/right flanking length, i2 
+
 
 chr_name = {}
 chr_name_revise = {}
@@ -41,10 +42,10 @@ for k in chr_name:
 #print(chr_name_revise)
 for k in chr_name_revise:
 	if len(chr_name_revise[k]) <= 1:
-		if chr_name_revise[k][0][0] > 2000:
-			chr_name_flanking[k] = [[chr_name_revise[k][0][0]-2000, chr_name_revise[k][0][1]+2000, chr_name_revise[k][0][2], chr_name_revise[k][0][3]]]
+		if chr_name_revise[k][0][0] > i2:
+			chr_name_flanking[k] = [[chr_name_revise[k][0][0]-i2, chr_name_revise[k][0][1]+i2, chr_name_revise[k][0][2], chr_name_revise[k][0][3]]]
 		else:
-			chr_name_flanking[k] = [[chr_name_revise[k][0][0], chr_name_revise[k][0][1]+2000, chr_name_revise[k][0][2], chr_name_revise[k][0][3]]]
+			chr_name_flanking[k] = [[chr_name_revise[k][0][0], chr_name_revise[k][0][1]+i2, chr_name_revise[k][0][2], chr_name_revise[k][0][3]]]
 	else:
 		for n in range(0, len(chr_name_revise[k])):
 			left1 = chr_name_revise[k][n][0]
@@ -54,17 +55,17 @@ for k in chr_name_revise:
 				left2 = chr_name_revise[k][n+1][0]
 				right2 = chr_name_revise[k][n+1][1]
 				orient2 = chr_name_revise[k][n+1][2]
-				if left2 - right1 <= 4000 and orient1 == orient2:
+				if left2 - right1 <= i2*2 and orient1 == orient2:
 					flank = int(math.floor((left2 - right1)/2))
-					if chr_name_revise[k][n][0] > 2000:
-						chr_name_flanking[k].append([chr_name_revise[k][n][0]-2000, chr_name_revise[k][n][1]+flank, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
+					if chr_name_revise[k][n][0] > i2:
+						chr_name_flanking[k].append([chr_name_revise[k][n][0]-i2, chr_name_revise[k][n][1]+flank, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 					else:
 						chr_name_flanking[k].append([chr_name_revise[k][n][0], chr_name_revise[k][n][1]+flank, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 				else:
-					if chr_name_revise[k][n][0] > 2000:
-						chr_name_flanking[k].append([chr_name_revise[k][n][0]-2000, chr_name_revise[k][n][1]+2000, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
+					if chr_name_revise[k][n][0] > i2:
+						chr_name_flanking[k].append([chr_name_revise[k][n][0]-i2, chr_name_revise[k][n][1]+i2, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 					else:
-						chr_name_flanking[k].append([chr_name_revise[k][n][0], chr_name_revise[k][n][1]+2000, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
+						chr_name_flanking[k].append([chr_name_revise[k][n][0], chr_name_revise[k][n][1]+i2, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 			elif n != 0 and n != len(chr_name_revise[k])-1:
 				left2 = chr_name_revise[k][n+1][0]
 				right2 = chr_name_revise[k][n+1][1]
@@ -73,32 +74,32 @@ for k in chr_name_revise:
 				right0 = chr_name_revise[k][n-1][1]
 				orient0 = chr_name_revise[k][n-1][2]
 				if orient0 == orient1 and orient1 == orient2:
-					if left1 - right0 > 4000:
-						flank_left = 2000
-					elif left1 - right0 <= 4000:
+					if left1 - right0 > i2*2:
+						flank_left = i2
+					elif left1 - right0 <= i2*2:
 						flank_left = int(math.floor((left1 - right0)/2))
-					if left2 - right1 > 4000:
-						flank_right = 2000
-					elif left2 - right1 <= 4000:
+					if left2 - right1 > i2*2:
+						flank_right = i2
+					elif left2 - right1 <= i2*2:
 						flank_right = int(math.floor((left2 - right1)/2))
 					chr_name_flanking[k].append([chr_name_revise[k][n][0]-flank_left, chr_name_revise[k][n][1]+flank_right, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 				elif orient0 != orient1 and orient1 == orient2:
-					flank_left = 2000
-					if left2 - right1 > 4000:
-						flank_right = 2000
-					elif left2 - right1 <= 4000:
+					flank_left = i2
+					if left2 - right1 > i2*2:
+						flank_right = i2
+					elif left2 - right1 <= i2*2:
 						flank_right = int(math.floor((left2 - right1)/2))
 					chr_name_flanking[k].append([chr_name_revise[k][n][0]-flank_left, chr_name_revise[k][n][1]+flank_right, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 				elif orient0 == orient1 and orient1 != orient2:
-					if left1 - right0 > 4000:
-						flank_left = 2000
-					elif left1 - right0 <= 4000:
+					if left1 - right0 > i2*2:
+						flank_left = i2
+					elif left1 - right0 <= i2*2:
 						flank_left = int(math.floor((left1 - right0)/2))
-					flank_right = 2000
+					flank_right = i2
 					chr_name_flanking[k].append([chr_name_revise[k][n][0]-flank_left, chr_name_revise[k][n][1]+flank_right, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 				elif orient0 != orient1 and orient1 != orient2:
-					flank_left = 2000
-					flank_right = 2000
+					flank_left = i2
+					flank_right = i2
 					chr_name_flanking[k].append([chr_name_revise[k][n][0]-flank_left, chr_name_revise[k][n][1]+flank_right, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 					
 			elif n == len(chr_name_revise[k])-1:
@@ -106,18 +107,19 @@ for k in chr_name_revise:
 				right0 = chr_name_revise[k][n-1][1]
 				orient0 = chr_name_revise[k][n-1][2]
 				if orient0 == orient1:
-					if left1 - right0 > 4000:
-						flank_left = 2000
-					elif left1 - right0 <= 4000:
+					if left1 - right0 > i2*2:
+						flank_left = i2
+					elif left1 - right0 <= i2*2:
 						flank_left = int(math.floor((left1 - right0)/2))
-					flank_right = 2000
+					flank_right = i2
 					chr_name_flanking[k].append([chr_name_revise[k][n][0]-flank_left, chr_name_revise[k][n][1]+flank_right, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 				elif orient0 != orient1:
-					flank_left = 2000
-					flank_right = 2000
+					flank_left = i2
+					flank_right = i2
 					chr_name_flanking[k].append([chr_name_revise[k][n][0]-flank_left, chr_name_revise[k][n][1]+flank_right, chr_name_revise[k][n][2], chr_name_revise[k][n][3]])
 
 
 for k in chr_name_flanking:
 	for n in chr_name_flanking[k]:
 		print(k, 'NLR_Annotator\tNBSLRR', n[0], n[1], '.', n[2], '.', n[3], sep='\t')
+
