2736e1e58d3dc41b4a8f64a2ca5aeb1861978be8
HongboDoll <101861734+HongboDoll@users.noreply.github.com>
2023-01-01 16:23:28 +0100

    Add files via upload

--- NLR synteny plot/Prf synteny/work.sh
+++ NLR synteny plot/Prf synteny/work.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#sed -n '6879,6883p' Solanum_jamesii.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"1","red"}else{print $4,$1,$2,$3,$6,"1","blue"}}' > Solanum_jamesii.prf.bed
-#sed -n '17191,17195p' Solanum_cardiophyllumC509.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"2","red"}else{print $4,$1,$2,$3,$6,"2","blue"}}' > Solanum_cardiophyllumC509.prf.bed
-#sed -n '16682,16686p' Solanum_chacoense.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"3","red"}else{print $4,$1,$2,$3,$6,"3","blue"}}' > Solanum_chacoense.prf.bed
-#sed -n '21155,21159p' Solanum_cajamarquenseC534.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"4","red"}else{print $4,$1,$2,$3,$6,"4","blue"}}' > Solanum_cajamarquenseC534.prf.bed
-#sed -n '36878,36882p' Solanum_neorossii.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"5","red"}else{print $4,$1,$2,$3,$6,"5","blue"}}' > Solanum_neorossii.prf.bed
-#sed -n '15797,15801p' Solanum_candolleanum.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"6","red"}else{print $4,$1,$2,$3,$6,"6","blue"}}' > Solanum_candolleanum.prf.bed
-#sed -n '27593,27597p' Solanum_tuberosum_phurejaC118.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"7","red"}else{print $4,$1,$2,$3,$6,"7","blue"}}' > Solanum_tuberosum_phurejaC118.prf.bed
-#sed -n '22672,22676p' Solanum_tuberosum_stenotomumC093.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"8","red"}else{print $4,$1,$2,$3,$6,"8","blue"}}' > Solanum_tuberosum_stenotomumC093.prf.bed
+sed -n '6871,6896p' Solanum_jamesii.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"1","red"}else{print $4,$1,$2,$3,$6,"1","blue"}}' > Solanum_jamesii.prf.bed
+sed -n '17173,17205p' Solanum_cardiophyllumC509.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"2","red"}else{print $4,$1,$2,$3,$6,"2","blue"}}' > Solanum_cardiophyllumC509.prf.bed
+sed -n '16674,16692p' Solanum_chacoense.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"3","red"}else{print $4,$1,$2,$3,$6,"3","blue"}}' > Solanum_chacoense.prf.bed
+sed -n '21148,21163p' Solanum_cajamarquenseC534.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"4","red"}else{print $4,$1,$2,$3,$6,"4","blue"}}' > Solanum_cajamarquenseC534.prf.bed
+sed -n '36873,36887p' Solanum_neorossii.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"5","red"}else{print $4,$1,$2,$3,$6,"5","blue"}}' > Solanum_neorossii.prf.bed
+sed -n '15790,15810p' Solanum_candolleanum.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"6","red"}else{print $4,$1,$2,$3,$6,"6","blue"}}' > Solanum_candolleanum.prf.bed
+sed -n '27587,27605p' Solanum_tuberosum_phurejaC118.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"7","red"}else{print $4,$1,$2,$3,$6,"7","blue"}}' > Solanum_tuberosum_phurejaC118.prf.bed
+sed -n '22665,22683p' Solanum_tuberosum_stenotomumC093.bed |awk 'BEGIN{OFS="\t"}{if($4~/_NLR_/){print $4,$1,$2,$3,$6,"8","red"}else{print $4,$1,$2,$3,$6,"8","blue"}}' > Solanum_tuberosum_stenotomumC093.prf.bed
 
 ######### reverse compliment these species to avoid inversions
-awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,10788641-$4,10788641-$3,"-",$6,$7}else{print $1,$2,10788641-$4,10788641-$3,"+",$6,$7}}' Solanum_cardiophyllumC509.prf.bed2 > Solanum_cardiophyllumC509.prf.reverse.bed
-awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,27025086-$4,27025086-$3,"-",$6,$7}else{print $1,$2,27025086-$4,27025086-$3,"+",$6,$7}}' Solanum_cajamarquenseC534.prf.bed2 > Solanum_cajamarquenseC534.prf.reverse.bed
-#awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,13537352-$4,13537352-$3,"-",$6,$7}else{print $1,$2,13537352-$4,13537352-$3,"+",$6,$7}}' Solanum_neorossii.prf.bed > Solanum_neorossii.prf.reverse.bed && mv Solanum_neorossii.prf.bed Solanum_neorossii.prf.bed2
-#awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,28853671-$4,28853671-$3,"-",$6,$7}else{print $1,$2,28853671-$4,28853671-$3,"+",$6,$7}}' Solanum_tuberosum_phurejaC118.prf.bed > Solanum_tuberosum_phurejaC118.prf.reverse.bed && mv Solanum_tuberosum_phurejaC118.prf.bed Solanum_tuberosum_phurejaC118.prf.bed2
+awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,10788641-$4,10788641-$3,"-",$6,$7}else{print $1,$2,10788641-$4,10788641-$3,"+",$6,$7}}' Solanum_cardiophyllumC509.prf.bed > Solanum_cardiophyllumC509.prf.reverse.bed && mv Solanum_cardiophyllumC509.prf.bed Solanum_cardiophyllumC509.prf.bed2
+awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,27025086-$4,27025086-$3,"-",$6,$7}else{print $1,$2,27025086-$4,27025086-$3,"+",$6,$7}}' Solanum_cajamarquenseC534.prf.bed > Solanum_cajamarquenseC534.prf.reverse.bed && mv Solanum_cajamarquenseC534.prf.bed Solanum_cajamarquenseC534.prf.bed2
+awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,13537352-$4,13537352-$3,"-",$6,$7}else{print $1,$2,13537352-$4,13537352-$3,"+",$6,$7}}' Solanum_neorossii.prf.bed > Solanum_neorossii.prf.reverse.bed && mv Solanum_neorossii.prf.bed Solanum_neorossii.prf.bed2
+awk 'BEGIN{OFS="\t"}{if($5=="+"){print $1,$2,28853671-$4,28853671-$3,"-",$6,$7}else{print $1,$2,28853671-$4,28853671-$3,"+",$6,$7}}' Solanum_tuberosum_phurejaC118.prf.bed > Solanum_tuberosum_phurejaC118.prf.reverse.bed && mv Solanum_tuberosum_phurejaC118.prf.bed Solanum_tuberosum_phurejaC118.prf.bed2
 
 rm prf_8_potato.bed
 cat *.prf*bed > prf_8_potato.bed
