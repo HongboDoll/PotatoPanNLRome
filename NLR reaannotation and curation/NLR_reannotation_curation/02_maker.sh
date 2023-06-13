729b215e9fb945bfd004f76d48f0425205e8ab8f
HongboDoll <101861734+HongboDoll@users.noreply.github.com>
2023-06-13 17:40:29 +0200

    Update 02_maker.sh

--- NLR reaannotation and curation/NLR_reannotation_curation/02_maker.sh
+++ NLR reaannotation and curation/NLR_reannotation_curation/02_maker.sh
@@ -2,7 +2,7 @@
 
 date
 spe=C400
-ref=../NLR_2k_seq/${spe}.NLR.2k.fa
+ref=../NLR_4k_seq/${spe}.NLR.4k.fa
 ref2=${spe}_hifiasm.purged.filter.fa
 threads=52
 aug_spe=${spe}_braker
@@ -30,7 +30,7 @@ rm -rf ${spe}.maker.output
 
 awk '$2=="maker"' ${spe}.all.gff > ${spe}.purged.all.NLR.gff3
 
-../convert_NLR_gff_coordinates.py ../NLR_2k_gff/${spe}.NLR.2k.gff3 ${spe}.purged.all.NLR.gff3 > tt && mv tt ${spe}.purged.all.NLR.gff3
+../convert_NLR_gff_coordinates.py ../NLR_4k_gff/${spe}.NLR.4k.gff3 ${spe}.purged.all.NLR.gff3 > tt && mv tt ${spe}.purged.all.NLR.gff3
 
 /public/agis/huangsanwen_group/lihongbo/software/maker/bin/quality_filter.pl -a 0.5 ${spe}.purged.all.NLR.gff3 > ${spe}.purged.filter.NLR.gff3
 
